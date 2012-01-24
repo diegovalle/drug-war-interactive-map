e3a7f9def6d7d13b78acc721aacbc783c323bfff
Diego Valle-Jones <diegovalle@gmail.com>
2012-01-23 19:57:29 -0600

    minimize css and javascript

---
+++ compile-yui.sh
@@ -0,0 +1,9 @@
+cat  css/cartodb.css css/bootstrap.css css/interactive-drug-war.css css/tipsy.css > css/combined.min.css
+
+java -jar yuicompressor-2.4.7/build/yuicompressor-2.4.7.jar --type css css/combined.css > css/combined.min.css
+
+
+cat js/english_text.js js/jquery.zclip.js js/bootstrap-modal.js js/wax.g.js js/protovis.min.js js/tipsy.js js/jquery.tipsy.js js/drug-eradication-compiled.js js/google-map.js js/cartodb-gmapsv3.js > js/combined.js
+
+java -jar yuicompressor-2.4.7/build/yuicompressor-2.4.7.jar --type js js/combined.js > js/combined.min.js
+
