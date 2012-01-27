b74a28deab3bc21f53dbbe77cce0c521a4248a5a
Diego Valle-Jones <diegovalle@gmail.com>
2012-01-26 21:22:26 -0600

    free the resource

--- shorten.php
+++ shorten.php
@@ -8,9 +8,12 @@
     $api_key = "api_key";
 
     //send it to the bitly shorten webservice
-    $ch = curl_init ("http://api.bitly.com/v3/shorten?login=$api_user&apiKey=$api_key&longUrl=$url&format=json");
+    $ch = curl_init ("http://api.j.mp/v3/shorten?login=$api_user&apiKey=$api_key&longUrl=$url&format=json");
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 
     //the response is a JSON object, send it to the webpage with a callback
     echo $_GET['jsoncallback'] . '(' . curl_exec($ch) . ');';
+
+    // close cURL resource, and free up system resources
+    curl_close($ch);
 ?>
