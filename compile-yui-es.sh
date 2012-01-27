a2400b8527d02b1eb56ace227c625078198134cc
Diego Valle-Jones <diegovalle@gmail.com>
2012-01-26 20:50:50 -0600

    adds nicer image legens in Spanish and English

--- compile-yui-es.sh
+++
@@ -1,4 +0,0 @@
-cat js/spanish_text.js js/jquery.zclip.js js/bootstrap-modal.js js/wax.g.js js/protovis.min.js js/tipsy.js js/jquery.tipsy.js js/drug-eradication-compiled.js js/google-map.js js/cartodb-gmapsv3.js > js/combined-es.js
-
-java -jar yuicompressor-2.4.7/build/yuicompressor-2.4.7.jar --type js js/combined-es.js > js/combined-es.min.js
-
--- compile-yui.sh
+++ compile-yui.sh
@@ -5,5 +5,5 @@ java -jar yuicompressor-2.4.7/build/yuicompressor-2.4.7.jar --type css css/combi
 
 cat js/english_text.js js/jquery.zclip.js js/bootstrap-modal.js js/wax.g.js js/protovis.min.js js/tipsy.js js/jquery.tipsy.js js/drug-eradication-compiled.js js/google-map.js js/cartodb-gmapsv3.js > js/combined.js
 
-java -jar yuicompressor-2.4.7/build/yuicompressor-2.4.7.jar --type js js/combined.js > js/combined.min.js
+java -jar compiler/compiler.jar --js=js/combined.js  --js_output_file=js/combined.min.js
 
